ab954f769cdab38c591ec227320cb6bf58bfeba0
Chris <cpm4@aber.ac.uk>
2013-06-06 01:55:48 +0100

    wordladder reading files of right length, issue with 2 digit numbers

--- wordLadder/fileIn.c
+++ wordLadder/fileIn.c
@@ -6,7 +6,7 @@
 void readFile(int lengthOfWord);
 
 /*array of strings at length of word + 1 for null terminater*/
-//char words[lengthOfWord+1][numberOfWords];
+//char words[][];
 
 int main()
 {
@@ -19,32 +19,64 @@ int main()
 	length = strlen(word);
 
 	printf("Word is: %s, Length of word is: %d\n", word, length);
+	
+	readFile(length);
 }
 
 
 /*Reads in file and stores in an array of strings*/
 void readFile(int lengthOfWord)
 {
-	char fileName[10];
-	char length[2];
+	printf("%d", lengthOfWord);
+
+	char fileName[15];
+	char length[5];
+	char *words[99999][lengthOfWord+1];		   //sets array size to really large
 	char *word;
 	FILE *fp;
+	int i = 0;
+
+	memset(fileName, 0, 15*(sizeof(fileName[0])));	    //clears array
+
+	//FIX ISSUE WITH NOT CONVERTING 2 DIGIT NUMBERS//
 
 	//convert int to string
 	sprintf(length, "%d", lengthOfWord);
-	printf("%s", length);
+	printf("temp:%s\n", length);
 
-	strcpy(fileName, "dict");
-	strcpy(fileName, length);
-	strcpy(fileName, ".dat");
-	
+	printf("%s",fileName);
+	strcat(fileName, "dict");
+	printf("1:%s\n", fileName);
+	strcat(fileName, length);
+	printf("2:%s\n", fileName);
+	strcat(fileName, ".dat");
+	printf("3:%s\n", fileName);
+		
+/*
 	fp = fopen(fileName, "r");
 
-	if(fp = NULL)
+	if(fp != NULL)
 	{
-		printf("File could not be opened");
-		exit(0);
-	}else{
-	}
+		char word[lengthOfWord+1];
 
+		while(fgets(word, sizeof(word), fp) != NULL)
+		{
+			fputs(word, stdout);
+		}
+		fclose(fp);
+	}else{
+		perror(fileName);
+	}
+*/
+//	fclose(fp);
 }
+
+
+
+
+
+
+
+
+
+
