20764d150b00ea4d2d72d384755bbc172ac9427c
mentalmarriott <firemazza@gmail.com>
2013-07-05 11:05:42 +0100

    got storing, passing and printing array sorted

--- wordLadder/fileIn.c
+++ wordLadder/fileIn.c
@@ -4,12 +4,11 @@
 
 //method declarations
 void readFile(int lengthOfWord);
-void printArray(char** wordList);
+void printArray(char** wordList, int arraySize);
 
 /*array of strings at length of word + 1 for null terminater*/
 //char words[][];
 
-char **wordsArray;
 
 /**
 * Start of the program where user enters a word and that word is then passed
@@ -42,18 +41,16 @@ void readFile(int lengthOfWord)
 	int j;
 
 	//creates a 2d array for words to go in
-	wordsArray = (char**)malloc(9999*sizeof(char*));
-	for(j = 0; j < 9999; j++)
+	/*for(j = 0; j < 9999; j++)
 	{
-		wordsArray[j] = (char*) malloc(wordLength*sizeof(char));
-	}
-//	wordsArray[9999][lengthOfWord+1];		   //sets array size to really large
+		wordsArray[j] = (char*) malloc(wordLength*sizeof(char));	//each item has space for length of given word
+	}*/
+	
 	char *word;
-	//FILE *fp;
 	char wordsList[wordLength+1][999];
 
 	memset(fileName, 0, 15*(sizeof(fileName[0])));	    //clears array
-	memset(wordsArray, 0, 999*(sizeof(char))); 
+	//memset(wordsArray, 0, 999*(sizeof(char))); 
 
 	//convert int to string
 	sprintf(length, "%d", wordLength);
@@ -68,8 +65,9 @@ void readFile(int lengthOfWord)
 	printf("3:%s\n", fileName);
 	
 	char line[wordLength+1];	
-	char **lines = NULL;
+	char **wordsArray = NULL;
 	int i = 0;
+	int arraySize = 0;
 
 	FILE *fp = fopen(fileName, "r");
 
@@ -77,14 +75,16 @@ void readFile(int lengthOfWord)
 	{
 		//char word[lengthOfWord+1];
 
-		while(fgets(line, wordLength+1, fp))
+		while(fgets(line, sizeof wordLength+1, fp))
 		{
 			i++;
-			lines = (char**)realloc(lines, sizeof(char*)*i);
-			lines[i-1] = strdup(line);
+			arraySize++;
+			wordsArray = (char**)realloc(wordsArray, sizeof(char*)*i);
+			//printf("Line %d is: %s\n", i, line);
+			wordsArray[i-1] = strdup(line);
 		}
 		fclose(fp);
-		printArray(lines);
+		printArray(wordsArray, arraySize);
 	}else{
 		perror(fileName);
 	}
@@ -96,16 +96,16 @@ void readFile(int lengthOfWord)
 /**
 * Prints array to make sure all words are added properly
 */
-void printArray(char** wordList)
+void printArray(char** wordsArray, int arraySize)
 {
 	int i;
 	int arrayLength;
 	
-	arrayLength = sizeof(wordList)/sizeof(char);
+	printf("Array length is: %d\n", arraySize);
 
-	for(i = 0; i < arrayLength; i++)
+	for(i = 0; i < 587; i++)
 	{
-		printf("%d: %s\n", i, wordList[i]);
+		printf("%d: %s\n", i, wordsArray[i]);
 	} 
 }
 
